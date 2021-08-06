1d65a27453fb3220b5a782e909c0429f442dcde6
andeen171 <zeander21@outlook.com.br>
2021-08-06 17:36:47 -0300

    Ver 2.0 beta full features implemented, only front-end styling needed now

--- user/forms.py
+++ user/forms.py
@@ -3,3 +3,8 @@ from django import forms
 
 class UploadPhotoForm(forms.Form):
     image = forms.FileField(label='Select a profile picture')
+
+
+class EditVideoInfoForm(forms.Form):
+    title = forms.CharField(label='Title')
+    description = forms.CharField(label='Description')
